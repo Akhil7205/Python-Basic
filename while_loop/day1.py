18285f45ee81601c3761580f034eb8ca7a21265b
AKhil Shinde <akhilshinde13401@gmail.com>
2025-03-16 12:41:56 +0530

    whileloop Q add

---
+++ while_loop/day1.py
@@ -0,0 +1,90 @@
+# Q1)Print Numbers from 1 to 10:
+# Write a program to print numbers from 1 to 10 using a while loop.
+
+# num=0
+# while(num<10):
+#     print(num,'0')
+#     num+=1
+#     print(num)
+
+#  Q2)Print Even Numbers from 2 to 20:
+# Write a program that prints even numbers from 2 to 20 using a while loop.
+
+# num=1
+# nl=20
+# while(num<nl+1):
+#     if num%2==0:
+#         print(num)
+#     num+=1
+    # print(num)
+
+# Q3) Sum of First N Natural Numbers:
+# Take a number N as input and calculate the sum of the first N natural numbers using a while loop.
+
+
+# N=int(input('enter no'))
+
+# a=N(N+1)/2
+# a=  (N*N+1)/2
+# print(a)
+# while(N>0):
+#    if
+
+# Q4)✅ Reverse Counting from 10 to 1:
+# Write a program to print numbers from 10 down to 1 using a while loop.
+
+# a=10
+# while(a>0):
+#    print(a)
+#    a-=1
+
+
+# Q5)✅ Multiplication Table:
+# Take a number as input and print its multiplication table using a while loop.
+
+# num=int(input('enter no'))
+# n=0
+# while(n<10):
+#    n+=1
+#    print(f'{num}*{n}=',num*n)
+
+
+# Q7)Take no and print table till that no print horizontal where 
+# num=int(input('enter no:'))
+# nu=0
+# while(nu<num):
+#     nu+=1
+#     n=0
+#     while(n<10):
+#         # num+=1
+#         n+=1
+#         print(f'{nu}*{n}=',nu*n, end="\t" )
+#     print()
+
+# Q8)take input and print no from 0 to input and input value to 0 first should be go from 5-0 and 0-5
+# num=int(input('enter no'))
+# newnum=num
+# n=0
+# while(num>n):
+#     n+=1
+#     print(n)
+
+# while(num>=0):
+#     print(num)
+#     num-=1
+
+# Q9)✅ Reverse a Number:
+# Take a number as input and print its reverse. (e.g., 123 → 321).
+# n='12387'
+# i=len(n)
+# a=0
+# while i>0:
+#     a=n[i]
+#     i-=i
+#     print(a)
+
+ab=8981
+asd=ab%10
+print(asd)
+# while(ahs>0):
+#     print()
